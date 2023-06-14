8ccefb57056133a025773ac0185444c01ae7f924
Marlu Patrocinio <marlupatrocinio01@gmail.com>
2023-06-14 10:34:26 -0300

    Seller entity class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,96 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable{
+	
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private Date birthDate;
+	private Double baseSalary;
+
+	
+	private Department department;
+	
+	public Seller() {
+	}
+
+	public Seller(Integer id, String name, Date birthDate, Double baseSalary, Department department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", baseSalary=" + baseSalary
+				+ ", department=" + department + "]";
+	}
+	
+	
+}
